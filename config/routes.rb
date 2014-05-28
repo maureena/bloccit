8ca07e6ae9020e38136f37c1ae6e366187e9e8bd
vagrant <vagrant@rails-dev-box>
2014-05-28 14:37:00 +0000

    Associated and nested topics and posts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Bloccit::Application.routes.draw do
 
   devise_for :users
-  resources :posts
 
+  resources :topics do
+    resources :posts, except: [:index]
+  end
+  
   get 'about' => "welcome#about"
 
   root to: 'welcome#index'
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,21 @@ require 'faker'
 end
 users = User.all
 
+
+# Create Topics
+15.times do
+  Topic.create(
+    name:         Faker::Lorem.sentence,
+    description:  Faker::Lorem.paragraph
+  )
+end
+topics = Topic.all
+
 # Create Posts
 50.times do
   Post.create(
-    user:   users.sample, 
+    user:   users.sample,
+    topic:  topics.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
   )
@@ -31,10 +42,35 @@ posts = Post.all
   )
 end
 
-User.first.update_attributes(
-  email:    'maureen.adamo@gmail.com',
+# Create an admin user
+admin = User.new(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+  )
+admin.skip_confirmation!
+admin.save
+
+# Create a moderator
+moderator = User.new(
+  name:     'Moderator User',
+  email:    'moderator@example.com', 
+  password: 'helloworld',
+  role:     'moderator'
+)
+moderator.skip_confirmation!
+moderator.save
+
+# Create a member
+member = User.new(
+  name:     'Member User',
+  email:    'member@example.com',
   password: 'helloworld',
 )
+member.skip_confirmation!
+member.save
+
 
 puts "Seed finished"
 puts "#{User.count} users created"
