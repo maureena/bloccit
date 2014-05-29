a79394a6ca2c03a55343cbfb2ecd960a9d7b71d2
vagrant <vagrant@rails-dev-box>
2014-05-29 03:22:52 +0000

    Fixed typo in setup_mail_production.

--- config/initializers/setup_mail_production.rb
+++ config/initializers/setup_mail_production.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address:          'smtp.sendgrid.net',
-    port:             '587'
+    port:             '587',
     authentication:   :plain,
     user_name:        ENV['SENDGRID_USERNAME'],
     password:         ENV['SENDGRID_PASSWORD'],
